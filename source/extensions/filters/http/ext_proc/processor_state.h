2deb25ea961bed6fd8c77e41c79120f7474fe3ce
Jacob Bohanon <jacob.bohanon@solo.io>
2024-01-23 19:08:25 -0500

    ext_proc: send attributes (#31090)
    Introduce the ability to send attributes in the External Processing Request
    
    ---------
    
    Signed-off-by: Jacob Bohanon <jacob.bohanon@solo.io>

--- source/extensions/filters/http/ext_proc/processor_state.h
+++ source/extensions/filters/http/ext_proc/processor_state.h
@@ -157,6 +157,8 @@ public:
   virtual envoy::service::ext_proc::v3::HttpTrailers*
   mutableTrailers(envoy::service::ext_proc::v3::ProcessingRequest& request) const PURE;
 
+  virtual StreamInfo::StreamInfo& streamInfo() PURE;
+
 protected:
   void setBodyMode(
       envoy::extensions::filters::http::ext_proc::v3::ProcessingMode_BodySendMode body_mode);
@@ -268,6 +270,8 @@ public:
   void requestWatermark() override;
   void clearWatermark() override;
 
+  StreamInfo::StreamInfo& streamInfo() override { return decoder_callbacks_->streamInfo(); }
+
 private:
   void setProcessingModeInternal(
       const envoy::extensions::filters::http::ext_proc::v3::ProcessingMode& mode);
@@ -340,6 +344,8 @@ public:
 
   void requestWatermark() override;
   void clearWatermark() override;
+
+  StreamInfo::StreamInfo& streamInfo() override { return encoder_callbacks_->streamInfo(); }
 
 private:
   void setProcessingModeInternal(
